88a80e70df0428b3af954b57b857cd948e0cc76e
BlowFly <blowfly0258@gmail.com>
2021-11-24 11:19:49 +0900

    published

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import Home from "./routes/Home"
 import './App.css'
-import { HashRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import About from './routes/About'
 import Navigation from "./components/Navigator"
 function App(){
     return (
-    <HashRouter>
+    <BrowserRouter>
         <Navigation />
         <Route PATH='./' exact={true} component={Home}/>
         <Route PATH='./about' component={About}/>npm 
         {/* <Route path='/movie-detail' component={detail}/> */}
-    </HashRouter>
-    )
-    
-        
+    </BrowserRouter>
+    )    
 }
-// { <Route path='/home'>
-// <h1>Home</h1>    
-// </Route>
-// <Route path='/home/instroduction'>
-// <h1>instroduction</h1>
-// </Route>
-// <Route path='/about'>
-// <h1>about</h1>
-// </Route> }
+
 export default App;
